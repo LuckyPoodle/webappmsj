d1f37de32d6e8553b4154db9900d8c764da15d5a
truffletou <truffletou@gmail.com>
2022-08-19 09:41:07 +0800

    mobile UI edit

--- next.config.js
+++ next.config.js
@@ -8,7 +8,8 @@ module.exports ={
     mapbox_key: "pk.eyJ1IjoiaGFwcHlqdWkiLCJhIjoiY2wwNjlxbmRxMDR4dTNrb2N0d3h3bnczdyJ9.OgYyQrRW6vkQ_VV4x1MKiQ",
     contentfulaccesstoken:"gzm8mYARoFaFlfeQWWMibFNCSKKBqQzegQBsnbpxbtg",
     contentfulspaceid:"nq1x3bfv0pp7",
-  
+    algoliaAdminApiKey:'65ca633d589f2e767de76b207c09d5a5',
+    algoliaApp:'K0O3Z1XZV4',
     NEXT_STRIPE_KEY:"pk_test_51HcYnSCzQqyN0N2r04TJVJCfdYRSYiK43EMVQQeqQFRBIk6zcOYYxWEZiKjzOJKCEjSdPCPNVdMBrKjuieAQ3vdt00AQuJjQfH"
  
             
