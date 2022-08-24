8593dacd88b20c30c2ca9ec810b375af83c30203
truffletou <truffletou@gmail.com>
2022-08-24 14:04:14 +0800

    minor edit

--- pages/index.js
+++ pages/index.js
@@ -320,7 +320,7 @@ export default function Home({ products }) {
           
 
           {/* on mobile grid has 1 column, sm screen 2 col, and so on */}
-         {obtainedProducts.length>0? <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+         {obtainedProducts.length>0? <div className="pt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {obtainedProducts?.map((pdt) => (
 
               <SmallCard
