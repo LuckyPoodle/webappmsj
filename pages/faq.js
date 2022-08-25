c4da52c5e7e90c6c17322eda37e498fc67f7ba13
truffletou <truffletou@gmail.com>
2022-08-25 12:46:45 +0800

    minor edit

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+
 
 const FAQ = () => {
     const [show, setShow] = useState(false);
@@ -184,7 +184,7 @@ const FAQ = () => {
 
             </div>
 
-            <Footer />
+
         </div>
 
 
